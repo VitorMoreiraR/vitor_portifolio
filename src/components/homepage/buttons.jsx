add48e816705c7577d3db314e208da7be5b1fe41
Vitor Moreira Ramos de Rezende <111029109+VitorMoreiraR@users.noreply.github.com>
2024-10-23 07:05:19 -0300

    Update buttons.jsx

--- src/components/homepage/buttons.jsx
+++ src/components/homepage/buttons.jsx
@@ -5,7 +5,7 @@ export default function Buttons () {
     return (
         <div className={`${styles.conjunto_botoes} ${styles.animation_botao_csv}`}>
             <div className={`${styles.botao_csv} ${styles.animation_botoes}`}>
-                <a href="/Curriculo_Vitor.pdf" download={'Curriculo_Vitor.pdf'}>
+                <a href="./Curriculo_Vitor.pdf" download={'Curriculo_Vitor.pdf'}>
                     <p>Download CV</p>
                     <img src="./icons/baixar.svg" alt="icone baixar"/>
                 </a>
